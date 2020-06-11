2c69d9942ece37ffbf5fc009d5cb885daacabe78
Andrew McAdams <andmcadams@gmail.com>
2020-06-11 16:39:00 -0500

    Refactor example test file

--- src/test/java/com/weightcalc/WeightCalcPluginTest.java
+++ src/test/java/com/weightcalc/WeightCalcPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.weightcalc;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class WeightCalcPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(WeightCalcPlugin.class);
 		RuneLite.main(args);
 	}
 }
